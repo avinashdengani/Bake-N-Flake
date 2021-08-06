468c6c46392ae36744aa938a8dc3a58868c0d13d
Avinash Dengani <avinashdengani123@gmail.com>
2021-08-06 13:55:59 +0530

    Added getters and delete image method in Category Model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getEditedDateAttribute()
+    {
+        if($this->updated_at == $this->created_at ) {
+            return null;
+        }
+        return $this->updated_at->diffForHumans();;
+    }
+    public function getImagePathAttribute()
+    {
+        return 'storage/' .$this->image;
+    }
+    public function deleteImage()
+    {
+        Storage::delete($this->image);
+    }
 }
