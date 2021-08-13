96f8f09b1567eb147398696b24b7012018e55261
Kapil Taparia <kapiltaparia22@gmail.com>
2021-08-13 13:42:59 +0530

    Created create.blade.php for products and added funtionality in ProductsController to return create page view

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -19,7 +19,9 @@ class ProductsController extends Controller
 
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('products.create', compact(['categories']));
+
     }
 
     public function store(Request $request)
