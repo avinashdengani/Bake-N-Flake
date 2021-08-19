8cb218067f1adcf70f8016785423cf0c2744bb64
Kapil Taparia <kapiltaparia22@gmail.com>
2021-08-19 12:05:47 +0530

    Created ImagePolicy and initialized in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use App\Models\Image;
 use App\Policies\CategoryPolicy;
+use App\Policies\ImagePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -16,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Category::class => CategoryPolicy::class,
+        Image::class => ImagePolicy::class,
     ];
 
     /**
