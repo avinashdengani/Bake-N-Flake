0c4f700cd346159133ce0210ec8bd99e7f4c6499
Kapil Taparia <kapiltaparia22@gmail.com>
2021-10-25 23:16:47 +0530

    Added a preloader, back to top button, included navbar and footer and added some styles.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>@yield('title')</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <link rel="icon" href= {{ asset('images/favico.ico') }}>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -32,26 +29,63 @@
 </head>
 <body>
     <div id="app">
+        <!-- Preloader -->
+        <div id="preloader">
 
+            <div id="status">&nbsp;
+                <img width="100%" src="{{asset('images/preloader.gif')}}" />
+            </div>
+        </div>
         @include('layouts.partials._navbar')
-
-        <main class="py-4 ">
-            <div class="m-5">
+        <a  href="#app"
+            title="GO TO TOP"
+            class="btn bg-purple text-white mb-5 mr-3 rounded-circle animate__animated  animate__slow "
+            id="back-to-top">
+        <i class="fa fa-arrow-up"></i>
+        </a>
+        <main class="">
+            <div class="p-4">
                 @include('layouts.partials._session-message')
             </div>
             <div class="d-flex justify-content-center flex-wrap">
                 @yield('sidebar')
                 @yield('content')
             </div>
+            @include('layouts.partials._footer')
         </main>
     </div>
 
-    <!-- JQUERY -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="{{asset('js/app.js')}}"></script>
 
     <!-- JQUERY VALIDATOR-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js" integrity="sha512-37T7leoNS06R80c8Ulq7cdCDU5MNQBwlYoy1TX/WUsLFC2eYNqtKlV0QjH7r8JpG/S0GUMZwebnVFLPd6SU5yg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    <script src="{{asset('js/MySmoothScroll.js')}}"></script>
+    <script>
+        $('#back-to-top').hide();
+        new MySmoothScroll("#back-to-top");
+    </script>
+    <script>
+        $(window).scroll(function () {
+                if ($(this).scrollTop() > 60) {
+                    $('#back-to-top').fadeIn('2000');
+                    $('.navbar').addClass('shadow-sm');
+                    $('#back-to-top').removeClass('animate__backOutUp');
+                    $('#back-to-top').addClass('animate__backInDown');
+                } else {
+                    $('#back-to-top').removeClass('animate__backInDown');
+                    $('#back-to-top').addClass('animate__backOutUp');
+                    $('.navbar').removeClass('shadow-sm');
+                }
+            });
+    </script>
+        <script>
+            $(window).on('load', function() {
+                $('#status').delay(1000).fadeOut();
+                $('#preloader').delay(1000).fadeOut('slow');
+                $('body').delay(1000).css({'overflow':'visible'});
+            });
+        </script>
     @yield('scripts')
 </body>
 </html>
